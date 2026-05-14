a8d8e3c87bacb0edfd30d272b9660a2be860fc8a
dwest77a <daniel.westwood@stfc.ac.uk>
2026-05-14 09:45:04 +0100

    Added new test functions

--- cci_facet_scanner/tests/test_consistency.py
+++ cci_facet_scanner/tests/test_consistency.py
@@ -17,6 +17,13 @@ class TestConsistency:
 
         assert 1==1,"Import collection handlers successful"
 
+    def test_file_handlers(self):
+        from cci_facet_scanner.file_handlers.base import FileHandler
+        from cci_facet_scanner.file_handlers.handler_factory import HandlerFactory
+        from cci_facet_scanner.file_handlers.netcdf import NetcdfHandler
+
+        assert 1==1,"Import file handlers successful"
+
     def test_core(self):
         from cci_facet_scanner.core.elasticsearch_connection import \
             ElasticsearchConnection
@@ -28,14 +35,31 @@ class TestConsistency:
         from cci_facet_scanner.scripts.facet_scanner_cmd import FacetExtractor
         from cci_facet_scanner.scripts.lotus_facet_scanner import \
             LotusFacetScanner
-        
+        from cci_facet_scanner.scripts.check_json import \
+            TextColours, TestJSONFile, TestResults, test_results
+        from cci_facet_scanner.scripts.check_tags import \
+            Dataset, main
+        from cci_facet_scanner.scripts.command_line_client import \
+            CCITaggerCommandLineClient, get_datasets_from_file, \
+            get_logging_level, read_json_file
+        from cci_facet_scanner.scripts.dump_facet_object import \
+            get_args, main
+
         assert 1==1, "Import scripts successful"
 
+    def test_tagging(self):
+        from cci_facet_scanner.tagging.dataset import Dataset
+        from cci_facet_scanner.tagging.facets import Concept, Facets
+        from cci_facet_scanner.tagging.tagger import ProcessDatasets
+
+        assert 1==1, "Import tagging classes successful"
+
     def test_utils(self):
-        from cci_facet_scanner.utils.snippets import (Singleton,
-                                                      generator_grouper,
-                                                      parse_key, query_yes_no,
-                                                      remove_quotes,
-                                                      split_outside_quotes)
+        from cci_facet_scanner.utils import constants
+        from cci_facet_scanner.utils import dataset_jsons
+        from cci_facet_scanner.utils import decorators
+        from cci_facet_scanner.utils import elasticsearch
+        from cci_facet_scanner.utils import settings
+        from cci_facet_scanner.utils import snippets
 
         assert 1==1, "Import utils successful"
